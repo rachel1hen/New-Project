661d9eec9e16c2d0b5ebaea82c653ba4e70f9f05
rachel1hen <race2towin@gmail.com>
2025-08-21 16:44:26 +0530

    Update test_bark.py

--- test_bark.py
+++ test_bark.py
@@ -1,6 +1,16 @@
 # import os
 # os.environ["SUNO_USE_SMALL_MODELS"] = "True"
 # os.environ["SUNO_OFFLOAD_CPU"] = "True"
+speaker =[
+     "v2/en_speaker_9",
+     "v2/en_speaker_1",
+     "v2/en_speaker_3",
+     "v2/en_speaker_5",
+     "v2/en_speaker_6",
+     "v2/en_speaker_7",
+     "v2/en_speaker_11",
+     "v2/en_speaker_13"
+]
 
 from bark import SAMPLE_RATE, generate_audio, preload_models
 from scipy.io.wavfile import write as write_wav
@@ -16,13 +26,16 @@ text_prompt = """
      Hello, my name is Suno. [clears throat] And, uh — and I like pizza.
      But I also have other interests such as playing tic tac toe.
 """
-audio_array = generate_audio(text_prompt, history_prompt=history_prompt)
+for speak in speaker:
+     audio_array = generate_audio(text_prompt, history_prompt=speak)
+     filename = f"{speak.replace('/','_')}.wav"
+     write_wav(filename, SAMPLE_RATE,audio_array)
 #audio_int16 = np.int16(audio_array / np.max(np.abs(audio_array)) * 32767)
 
 # save audio to disk
 #write_wav("bark_generation.wav", SAMPLE_RATE, audio_int16)
 # save audio to disk
-write_wav("bark_generation.wav", SAMPLE_RATE, audio_array)
+#write_wav("bark_generation.wav", SAMPLE_RATE, audio_array)
 
 print("✅ Audio generated and saved to bark_generation.wav")
 
